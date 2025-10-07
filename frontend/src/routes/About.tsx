5ca104befd6a5fd4912341b32687bce6a01c24d3
Melody Cartwright <melodycartwright@hotmail.com>
2025-10-07 17:43:14 +0200

    folder restructuring, seperation of concern

--- frontend/src/routes/About.tsx
+++ frontend/src/routes/About.tsx
@@ -2,15 +2,15 @@ import AboutHero from "../sections/about/AboutHero";
 //import AboutStory from "../sections/about/AboutStory";
 //import AboutSkills from "../sections/about/AboutSkills";
 import AboutContent from "../sections/about/AboutContent";
-import AboutLinks from "../sections//about/AboutLinks";
+import AboutLinks from "../sections/about/AboutLinks";
 
 export default function About() {
   return (
     <div className="bg-softwhite dark:bg-charcoal text-charcoal dark:text-white">
       <AboutHero />
-     
+
       <AboutContent />
-<AboutLinks />
+      <AboutLinks />
     </div>
   );
 }
