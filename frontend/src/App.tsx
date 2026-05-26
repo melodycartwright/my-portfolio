deeadd90203e65fc70f205eaacd1d697c14b3020
Melody Cartwright <melody94c@gmail.com>
2026-05-26 14:19:03 +0200

    feat(frontend): add blog route and placeholder page wiring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import Navbar from "./sections/shared/NavBar";
 import Home from "./routes/Home";
-import About from "./routes/About"
+import About from "./routes/About";
 import Projects from "./routes/Projects";
 import Blog from "./routes/Blog";
 import Contact from "./routes/Contact";
